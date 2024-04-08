63c13684e8cbe7b6b6ba88d06ae51fa0194b2a43
Yasmin Cordeiro <ymeira@ecomp.uefs.br>
2024-04-08 14:13:13 -0300

    feat(src/common): Update algorithm.c file

--- src/common/algorithm.c
+++ src/common/algorithm.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int verifyAlocation(const int* matrix) {
-    if(matrix == NULL) {
-        printf("Erro: Falha ao alocar memória\n");
-        return EXIT_FAILURE;
-    }
-    return EXIT_SUCCESS;
-}
-
 char** initializeMatrix(int rows, int columns) {
 
     char **matrix = (char **)malloc(rows * sizeof(char *));
     if (matrix == NULL) {
-        printf("Error allocating memory for matrix rows\n");
+        printf("Erro: Falha ao alocar memória\n");
         exit(EXIT_FAILURE);
     }
 
     for (int i = 0; i < rows; ++i) {
         matrix[i] = (char *)malloc(columns * sizeof(char));
         if (matrix[i] == NULL) {
-            printf("Error allocating memory for matrix columns\n");
+            printf("Erro: Falha ao alocar memória\n");
             exit(EXIT_FAILURE);
         }
         for (int j = 0; j < columns; ++j) {
@@ -30,6 +22,9 @@ char** initializeMatrix(int rows, int columns) {
 
     return matrix;
 }
+
+
+
 
 
 
