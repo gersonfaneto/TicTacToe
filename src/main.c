b16ff6861980e6daa542f473ede9f13742ab0509
Yasmin Cordeiro <ymeira@ecomp.uefs.br>
2024-04-09 13:26:56 -0300

    feat(src): Update main.c

--- src/main.c
+++ src/main.c
@@ -3,27 +3,9 @@
 #include "common/defines.h"
 #include "utils/format.h"
 
-int verifyMainDiagonal(int dimension, char matrix[dimension][dimension], char playerCharacther) {
-    
-    int win;
-
-    for(int i = 0; i < dimension; ++i) {
-        win = 1;
-        if(matrix[i][i] != playerCharacther) {
-            win = 0;
-            break;
-        }
-    }
-    if(win) {
-        return 1;
-    }
-    return 0;
-}
-
-
 int main(void) {
   println(D_MSG);
-  char matrix[3][3] = {{'-', '-', '-'}, {'-', 'X', '-'}, {'-', '-', 'X'}};
-  printf("%d\n", verifyMainDiagonal(3, matrix, 'X'));
+  //char matrix[3][3] = {{'-', '-', 'X'}, {'-', 'X', '-'}, {'X', '-', '-'}};
+  //printf("%d\n", verifySecundaryDiagonal(3, matrix, 'X'));
   return 0;
 }
