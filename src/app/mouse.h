11f592e64f6433f2bc496445ba8dcfcb24ca7b8f
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-04-11 21:23:48 -0300

    feat(src/app): Add `mouse`.
    Module for reading, decoding and storing data from the mouse.
    
    Also added `config.h` for some compile time tweaks.

---
+++ src/app/mouse.h
@@ -0,0 +1,21 @@
+#ifndef MOUSE_H_
+#define MOUSE_H_
+
+#include "common/types.h"
+
+typedef struct mouse_t mouse_t;
+
+struct mouse_t {
+  i32_t fd;
+  i32_t x;
+  i32_t y;
+  i8_t left;
+  i8_t middle;
+  i8_t right;
+
+  void (*read)(mouse_t* mouse, i32_t rows, i32_t cols);
+};
+
+mouse_t init_mouse(i32_t x, i32_t y);
+
+#endif  // !MOUSE_H_
---
+++ src/config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define MOUSE_SENSIBILITY 2
+
+#define MOUSE_DEVICE_PATH "/dev/input/mice"
+
+#endif  // !CONFIG_H_
