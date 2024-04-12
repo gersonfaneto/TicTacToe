5abfce9c630673097f1a3a7c95c0834421d3e695
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-04-11 21:26:23 -0300

    feat!(src): Update `main`.

--- src/main.c
+++ src/main.c
@@ -1,11 +1,55 @@
+#include <signal.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include "common/defines.h"
-#include "utils/format.h"
+#include <unistd.h>
+
+#include "app/mouse.h"
+#include "app/window.h"
+#include "common/termctl.h"
+
+volatile i32_t is_running = 1;
+
+void interruption_handler(i32_t signal);
 
 int main(void) {
-  println(D_MSG);
-  //char matrix[3][3] = {{'-', '-', 'X'}, {'-', 'X', '-'}, {'X', '-', '-'}};
-  //printf("%d\n", verifySecundaryDiagonal(3, matrix, 'X'));
+  mouse_t mouse;
+  window_t window;
+
+  window = init_window();
+
+  mouse = init_mouse(window.cols, window.rows);
+
+  signal(SIGINT, interruption_handler);
+
+  enter_alt_screen();
+  disable_cursor();
+  disable_echo();
+
+  mouse.x = window.cols / 2;
+  mouse.y = window.rows / 2;
+
+  while (is_running) {
+    clear_screen();
+
+    printf_at_xy(0, 1, "Canvas :: [%d, %d]", window.cols, window.rows);
+    printf_at_xy(0, 2, "Mouse  :: [%d, %d]", mouse.x, mouse.y);
+    printf_at_xy(0, 3, "Clicks :: [%d, %d, %d]", mouse.left, mouse.middle, mouse.right);
+
+    printf_at_xy(mouse.x, mouse.y, "%c\n", '@');
+
+    mouse.read(&mouse, window.rows, window.cols);
+  }
+
+  exit_alt_screen();
+  enable_cursor();
+  enable_echo();
+
+  close(mouse.fd);
+
+  printf("Closing...\n");
+
   return 0;
+}
+
+void interruption_handler(i32_t signal) {
+  is_running = 0;
 }
