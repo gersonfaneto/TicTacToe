6815f0cf50fbc6796e19008a4b84c1cece3aef8d
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-04-11 21:22:20 -0300

    feat(src/utils): Add `utils`.
    Some nice UTF-8 characters for latter use on the game layout...

---
+++ src/utils/ui.h
@@ -0,0 +1,14 @@
+#ifndef UI_H_
+#define UI_H_
+
+#define UPPER_RIGHT_CORNER "┐"
+#define UPPER_LEFT_CORNER "┌"
+#define UPPER_SEGMENT "─"
+
+#define LOWER_RIGHT_CORNER "┘"
+#define LOWER_LEFT_CORNER "└"
+#define LOWER_SEGMENT "─"
+
+#define SIDE_SEGMENT "│"
+
+#endif  // !UI_H_
