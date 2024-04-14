e2c24a593260c1bff62a517245fbfe3f33deb1d1
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-04-14 18:42:32 -0300

    feat!(src): Update `main`.

--- src/main.c
+++ src/main.c
@@ -1,55 +1,46 @@
-#include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
 
-#include "app/mouse.h"
-#include "app/window.h"
+#include "app/handlers.h"
 #include "common/termctl.h"
+#include "state.h"
 
-volatile i32_t is_running = 1;
-
-void interruption_handler(i32_t signal);
+state_t state;
 
 int main(void) {
-  mouse_t mouse;
-  window_t window;
+  state.is_running = 1;
 
-  window = init_window();
+  state.mouse = init_mouse();
+  state.window = init_window();
 
-  mouse = init_mouse(window.cols, window.rows);
+  state.mouse.x = state.window.cols / 2;
+  state.mouse.y = state.window.rows / 2;
 
-  signal(SIGINT, interruption_handler);
+  set_handlers();
 
   enter_alt_screen();
   disable_cursor();
   disable_echo();
 
-  mouse.x = window.cols / 2;
-  mouse.y = window.rows / 2;
-
-  while (is_running) {
+  while (state.is_running) {
     clear_screen();
 
-    printf_at_xy(0, 1, "Canvas :: [%d, %d]", window.cols, window.rows);
-    printf_at_xy(0, 2, "Mouse  :: [%d, %d]", mouse.x, mouse.y);
-    printf_at_xy(0, 3, "Clicks :: [%d, %d, %d]", mouse.left, mouse.middle, mouse.right);
+    printf_at_xy(0, 1, "Canvas :: [%d, %d]", state.window.cols, state.window.rows);
+    printf_at_xy(0, 2, "Mouse  :: [%d, %d]", state.mouse.x, state.mouse.y);
+    printf_at_xy(0, 3, "Clicks :: [%d, %d, %d]", state.mouse.left, state.mouse.middle, state.mouse.right);
 
-    printf_at_xy(mouse.x, mouse.y, "%c\n", '@');
+    printf_at_xy(state.mouse.x, state.mouse.y, "%c\n", '@');
 
-    mouse.read(&mouse, window.rows, window.cols);
+    state.mouse.read(&state.mouse, state.window.rows, state.window.cols);
   }
 
   exit_alt_screen();
   enable_cursor();
   enable_echo();
 
-  close(mouse.fd);
+  close(state.mouse.fd);
 
   printf("Closing...\n");
 
   return 0;
-}
-
-void interruption_handler(i32_t signal) {
-  is_running = 0;
 }
