7d96416498a850f21d5351d0a8d316f921907d5c
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-04-14 19:22:38 -0300

    feat(src): Update `main`.
    Just playing around with the interface...

--- src/main.c
+++ src/main.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "app/handlers.h"
 #include "common/termctl.h"
 #include "state.h"
+
+#define PLAYER_X 0
+#define PLAYER_Y 1
+
+#define PLAYER_COMPONET_WIDTH 7
+#define PLAYER_COMPONET_HEIGHT 5
+
+const char* players[][PLAYER_COMPONET_HEIGHT] = {
+    {
+        "  ___  ",
+        " / _ \\ ",
+        "| | | |",
+        "| |_| |",
+        " \\___/ ",
+    },
+    {
+        "__  __ ",
+        "\\ \\/ / ",
+        " \\  /  ",
+        " /  \\  ",
+        "/_/\\_\\ ",
+    },
+};
 
 state_t state;
 
@@ -18,6 +42,8 @@ int main(void) {
 
   set_handlers();
 
+  printf("Starting...\n");
+
   enter_alt_screen();
   disable_cursor();
   disable_echo();
@@ -28,6 +54,20 @@ int main(void) {
     printf_at_xy(0, 1, "Canvas :: [%d, %d]", state.window.cols, state.window.rows);
     printf_at_xy(0, 2, "Mouse  :: [%d, %d]", state.mouse.x, state.mouse.y);
     printf_at_xy(0, 3, "Clicks :: [%d, %d, %d]", state.mouse.left, state.mouse.middle, state.mouse.right);
+
+    for (int i = 0; i < PLAYER_COMPONET_HEIGHT; ++i) {
+      if (state.mouse.left) {
+        printf_at_xy(((state.window.cols / 2) - (strlen(players[PLAYER_X][i]) / 2) - strlen(players[PLAYER_Y][i])),
+                     state.window.rows / 2 + i, "%s", players[PLAYER_X][i]);
+        printf_at_xy(((state.window.cols / 2) - (strlen(players[PLAYER_Y][i]) / 2) + strlen(players[PLAYER_X][i])),
+                     state.window.rows / 2 + i, "%s", players[PLAYER_Y][i]);
+      } else {
+        printf_at_xy(((state.window.cols / 2) - (strlen(players[PLAYER_Y][i]) / 2) - strlen(players[PLAYER_X][i])),
+                     state.window.rows / 2 + i, "%s", players[PLAYER_Y][i]);
+        printf_at_xy(((state.window.cols / 2) - (strlen(players[PLAYER_X][i]) / 2) + strlen(players[PLAYER_Y][i])),
+                     state.window.rows / 2 + i, "%s", players[PLAYER_X][i]);
+      }
+    }
 
     printf_at_xy(state.mouse.x, state.mouse.y, "%c\n", '@');
 
