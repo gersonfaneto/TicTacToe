f09de30ad0d75e86748810bc354df5c8dc816f57
Nathielle Alves <telly.cerq@gmail.com>
2024-04-15 11:41:08 -0300

    Create game_logic.c

---
+++ src/common/game_logic.c
@@ -0,0 +1,18 @@
+#include "common/algorithm.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char playerOne = 'x';
+    char playerTwo = 'o';
+
+    int rows = 3; // Número de linhas da matriz
+    int cols = 3; // Número de colunas da matriz
+
+    //variável que indica de qual jogador pertece a jogada atual (números pares - jogador x; números ímpares - jogador o)
+    int verifyPlayerTurn = 0;
+
+    //verifica se a matriz do tabuleiro está vazia para começar o jogo
+     
+}
