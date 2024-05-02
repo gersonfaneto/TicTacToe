e5413a952ab407398aa07dc0e641cec3feabd093
Yasmin <ymeira@ecomp.uefs.br>
2024-05-02 17:18:12 -0300

    feat: update algorithm.h

--- src/common/algorithm.h
+++ src/common/algorithm.h
@@ -2,11 +2,17 @@
 #define ALGORITHM_H_
 
 char **initializeMatrix(int rows, int columns);
+
 int isEmpty(int row, int column, char **matrix);
+
 int verifyRows(int dimension, char **matrix, char playerCharacther);
+
 int verifyColumns(int dimension, char **matrix, char playerCharacther);
+
 int verifyMainDiagonal(int dimension, char **matrix, char playerCharacther);
+
 int verifySecundaryDiagonal(int dimension, char **matrix, char playerCharacther);
+
 void showMatrix(int dimension, char **matrix);
 
 #endif  // !ALGORITHM_H_
