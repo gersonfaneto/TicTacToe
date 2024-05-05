c058e4e5e74d7b6d23612ba7409e555acec655e1
Yasmin <ymeira@ecomp.uefs.br>
2024-05-05 20:47:33 -0300

    feat: update appearance.h

--- src/app/appearance.h
+++ src/app/appearance.h
@@ -2,6 +2,7 @@
 #define APPEARANCE_H_
 
 #include "common/types.h"
+#include "utils/ui.h"
 
 typedef struct quadrant_t quadrant_t;
 
@@ -15,17 +16,19 @@ quadrant_t init_quadrant();
 typedef struct board_t board_t;
 
 struct board_t{
-    i8_t row0;
-    i8_t row1;
-    i8_t row2;
-    i8_t row3;
+    char matrix[DIMENSION][DIMENSION];
 
-    i8_t col0;
-    i8_t col1;
-    i8_t col2;
-    i8_t col3;
+    i8_t row0, row1, row2, row3;
+    i8_t col0, col1, col2, col3;
 };
 
 board_t init_board();
+
+/**
+ * Prints the matrix in the terminal.
+ * @param dimension The dimension of the matrix.
+ * @param board The matrix to be printed.
+ */
+void show_board(i8_t dimension, board_t board);
 
 #endif  // !APPEARANCE_H_
