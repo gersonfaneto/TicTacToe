50d06ce7cf8e1c384c94c4465c62ee48d6a44d64
Yasmin <ymeira@ecomp.uefs.br>
2024-05-05 20:48:13 -0300

    feat: update algorithm.c

--- src/common/algorithm.c
+++ src/common/algorithm.c
@@ -3,42 +3,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char **init_Matrix(i8_t rows, i8_t cols) {
-  char **matrix = (char **)malloc(rows * sizeof(char *));
-
-  if (matrix == NULL) {
-    printf("Erro: Falha ao alocar memória\n");
-    exit(EXIT_FAILURE);
-  }
-
-  for (i8_t i = 0; i < rows; ++i) {
-    matrix[i] = (char *)malloc(cols * sizeof(char));
-    if (matrix[i] == NULL) {
-      printf("Erro: Falha ao alocar memória\n");
-      exit(EXIT_FAILURE);
-    }
-    for (i8_t j = 0; j < cols; ++j) {
-      matrix[i][j] = '-';
+void init_matrix(board_t board, i8_t dimension) {
+  for(i8_t i = 0; i < dimension; ++i) {
+    for(i8_t j = 0; j < dimension; ++j) {
+      board.matrix[i][j] = '-';
     }
   }
-
-  return matrix;
 }
 
-int isEmpty(i8_t row, i8_t column, char **matrix) {
-  if (matrix[row][column] == '-') {
+int isEmpty(board_t board, i8_t row, i8_t column) {
+  if (board.matrix[row][column] == '-') {
     return 1;
   }
   return 0;
 }
 
-int verifyRows(i8_t dimension, char **matrix, char playerCharacther) {
-  int win;
+int verifyRows(board_t board, i8_t dimension, char playerCharacther) {
+  i8_t win;
 
   for (i8_t i = 0; i < dimension; ++i) {
     win = 1;
     for (i8_t j = 0; j < dimension; ++j) {
-      if (matrix[i][j] != playerCharacther) {
+      if (board.matrix[i][j] != playerCharacther) {
         win = 0;
         break;
       }
@@ -50,13 +36,13 @@ int verifyRows(i8_t dimension, char **matrix, char playerCharacther) {
   return 0;
 }
 
-int verifyColumns(i8_t dimension, char **matrix, char playerCharacther) {
-  int win;
+int verifyColumns(board_t board, i8_t dimension, char playerCharacther) {
+  i8_t win;
 
   for (i8_t i = 0; i < dimension; ++i) {
     win = 1;
     for (i8_t j = 0; j < dimension; ++j) {
-      if (matrix[j][i] != playerCharacther) {
+      if (board.matrix[j][i] != playerCharacther) {
         win = 0;
         break;
       }
@@ -68,12 +54,12 @@ int verifyColumns(i8_t dimension, char **matrix, char playerCharacther) {
   return 0;
 }
 
-int verifyMainDiagonal(i8_t dimension, char **matrix, char playerCharacther) {
-  int win;
+int verifyMainDiagonal(board_t board, i8_t dimension, char playerCharacther) {
+  i8_t win;
 
   for (i8_t i = 0; i < dimension; ++i) {
     win = 1;
-    if (matrix[i][i] != playerCharacther) {
+    if (board.matrix[i][i] != playerCharacther) {
       win = 0;
       break;
     }
@@ -84,11 +70,11 @@ int verifyMainDiagonal(i8_t dimension, char **matrix, char playerCharacther) {
   return 0;
 }
 
-int verifySecundaryDiagonal(i8_t dimension, char **matrix, char playerCharacther) {
-  int win = 1;
+int verifySecundaryDiagonal(board_t board, i8_t dimension, char playerCharacther) {
+  i8_t win = 1;
 
   for (i8_t i = 0; i < dimension; ++i) {
-    if (matrix[i][dimension - 1 - i] != playerCharacther) {
+    if (board.matrix[i][dimension - 1 - i] != playerCharacther) {
       win = 0;
       break;
     }
@@ -99,10 +85,3 @@ int verifySecundaryDiagonal(i8_t dimension, char **matrix, char playerCharacther
   return 0;
 }
 
-void showMatrix(i8_t dimension, char **matrix) {
-  for (i8_t i = 0; i<dimension; i++) {
-    for (i8_t j; j<dimension; j++) {
-      printf("%c \n", matrix[i][j]);
-    }
-  }
-}
