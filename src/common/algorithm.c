5c26b17846184a2509fb22c474e89cf349ef8e24
Yasmin <ymeira@ecomp.uefs.br>
2024-05-09 00:15:36 -0300

    feat: update algorithm.c

--- src/common/algorithm.c
+++ src/common/algorithm.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include "state.h"
 
+
 i8_t is_empty(board_t board, i8_t row, i8_t column) {
   if (board.matrix[row][column] == '-') {
     return 1;
@@ -26,12 +27,10 @@ i8_t check_win(board_t board, i8_t dimension, char playerCharacter) {
   i8_t main_diag_win = 1;
   i8_t sec_diag_win = 1;
 
-  // Verificar linhas, colunas e diagonais simultaneamente
   for (i8_t i = 0; i < dimension; ++i) {
     i8_t row_win = 1;
     i8_t col_win = 1;
 
-    // Verificar linhas e colunas
     for (i8_t j = 0; j < dimension; ++j) {
       if (board.matrix[i][j] != playerCharacter) {
           row_win = 0;
@@ -41,28 +40,24 @@ i8_t check_win(board_t board, i8_t dimension, char playerCharacter) {
       }
     }
 
-    // Se alguma linha ou coluna ganhou, retorna 1
     if (row_win || col_win) {
       return 1;
     }
 
-    // Verificar a diagonal principal
     if (board.matrix[i][i] != playerCharacter) {
       main_diag_win = 0;
     }
 
-    // Verificar a diagonal secundária
     if (board.matrix[i][dimension - 1 - i] != playerCharacter) {
       sec_diag_win = 0;
     }
   }
 
-  // Se alguma das diagonais ganhou, retorna 1
   if (main_diag_win || sec_diag_win) {
       return 1;
   }
 
-  return 0; // Nenhum ganhador encontrado
+  return 0; 
 }
 
 i8_t check_tie(board_t board, i8_t dimension) {
