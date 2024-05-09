2184050e4ad8de58bfbdd9369a67a7852fadf85c
Yasmin <ymeira@ecomp.uefs.br>
2024-05-09 00:06:31 -0300

    feat: update algorithm.h

--- src/common/algorithm.h
+++ src/common/algorithm.h
@@ -5,48 +5,12 @@
 #include "utils/ui.h"
 #include "app/appearance.h"
 
-//void init_matrix(board_t board, i8_t dimension);
-
 i8_t is_empty(board_t board, i8_t row, i8_t column);
 
-/*** 
-* This function checks if the player represented by the character 'playerCharacther' has won in any row of the matrix.
-* @param dimension The dimension of the matrix (number of rows/columns).
-* @param matrix The matrix to be checked.
-* @param playerCharacther The character representing the player.
-* @return 1 if the player has won, otherwise 0.
-*/
-i8_t verify_rows(board_t board, i8_t dimension, char playerCharacther);
+i8_t is_board_full(board_t board, i8_t dimension);
 
-/*** 
-* This function checks if the player has won in any column of the matrix.
-* @param dimension The dimension of the matrix (number of rows/columns).
-* @param matrix The matrix to be checked.
-* @param playerCharacther The character representing the player.
-* @return 1 if the player has won, otherwise 0.
-*/
-i8_t verify_columns(board_t board, i8_t dimension, char playerCharacther);
+i8_t check_win(board_t board, i8_t dimension, char playerCharacter);
 
-/**
- * Verifies if the player has won in the main diagonal of the matrix.
- * @param dimension The dimension of the matrix.
- * @param matrix The matrix to be checked.
- * @param playerCharacther The character representing the player.
- * @return Returns 1 if the player has won in the main diagonal, otherwise returns 0.
- */
-i8_t verify_main_diagonal(board_t board, i8_t dimension, char playerCharacther);
-
-/**
- * Verifies if the player has won in the secondary diagonal of the matrix.
- * @param dimension The dimension of the matrix.
- * @param matrix The matrix to be checked.
- * @param playerCharacther The character representing the player.
- * @return Returns 1 if the player has won in the secondary diagonal, otherwise returns 0.
- */
-i8_t verify_secundary_diagonal(board_t board, i8_t dimension, char playerCharacther);
-
-//void run_game(board_t* board, i8_t turns, i8_t* coordinates);
-
-i8_t check_win(board_t* board, i8_t dimension, char playerCharacter);
+i8_t check_tie(board_t board, i8_t dimension);
 
 #endif  // !ALGORITHM_H_
